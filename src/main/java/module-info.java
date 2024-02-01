5a6d2e799d74addd9381912a52c3e27bbde0ecbc
samda <samdaliri39@gmail.com>
2024-02-01 18:30:11 -0500

    Project first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.eecs2311termproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.eecs2311termproject to javafx.fxml;
+    exports com.example.eecs2311termproject;
+}
