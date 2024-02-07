cdce5d6b5a72125934d62b90568a4c9bb70b39b3
averybackus <averybackus64@gmail.com>
2024-02-07 17:35:17 -0500

    Avery first commit with food class

---
+++ src/main/java/com/example/eecs2311termproject/Food.java
@@ -0,0 +1,56 @@
+package com.example.eecs2311termproject;
+
+import java.util.ArrayList;
+
+public class Food {
+    protected int calories;
+    protected double protein;
+    protected double carbs;
+    protected double fat;
+    protected ArrayList<String> ingredients;
+    protected ArrayList<String> dietaryRestrictions;
+    protected ArrayList<String> allergies;
+
+    // Constructor
+    public Food(int calories, double protein, double carbs, double fat,
+                ArrayList<String> ingredients, ArrayList<String> dietaryRestrictions,
+                ArrayList<String> allergies) {
+        this.calories = calories;
+        this.protein = protein;
+        this.carbs = carbs;
+        this.fat = fat;
+        this.ingredients = ingredients;
+        this.dietaryRestrictions = dietaryRestrictions;
+        this.allergies = allergies;
+    }
+
+    // Getter methods
+    public int getCalories() {
+        return calories;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public double getCarbs() {
+        return carbs;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public ArrayList<String> getIngredients() {
+        return ingredients;
+    }
+
+    public ArrayList<String> getDietaryRestrictions() {
+        return dietaryRestrictions;
+    }
+
+    public ArrayList<String> getAllergies() {
+        return allergies;
+    }
+
+}
