6430c5048e6af540ee78c5d17b53777015805b9d
Raha-Golsorkhi <raha.golsorkhi1@gmail.com>
2024-02-25 23:09:23 -0500

    Testing PaymentHandler class

---
+++ src/test/java/com/example/eecs2311termproject/PaymentHandlerTest.java
@@ -0,0 +1,12 @@
+package com.example.eecs2311termproject;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class PaymentHandlerTest {
+
+    
+}
