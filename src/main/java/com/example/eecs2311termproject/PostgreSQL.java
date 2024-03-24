9095a13ae3c903525fac8943b530f4dfb82ee052
averybackus <averybackus64@gmail.com>
2024-03-24 01:23:19 -0400

    Completed all-you-can-eat option. Commented out media player code for the video as it did not work.

--- src/main/java/com/example/eecs2311termproject/PostgreSQL.java
+++ src/main/java/com/example/eecs2311termproject/PostgreSQL.java
@@ -6,7 +6,11 @@ public class PostgreSQL {
     public static void WriteToDatabase(String foodName, double price, int quantity){
         String url = "jdbc:postgresql:postgres";
         String user = "postgres";
-        String password = "shitijagg7";
+        String password = "";
+
+        if(ClientSide.AYCE){
+            price = 0;
+        }
 
         try (Connection con = DriverManager.getConnection(url, user, password)) {
             System.out.println("Connected to PostgreSQL database!");
@@ -36,13 +40,16 @@ public class PostgreSQL {
     public static void updateQuantity(String foodName, int quantityToAdd) {
         String url = "jdbc:postgresql:postgres";
         String user = "postgres";
-        String password = "Adrian";
+        String password = "";
+
+
 
         try (Connection con = DriverManager.getConnection(url, user, password)) {
             System.out.println("Connected to PostgreSQL database!");
 
             String selectQuery = "SELECT \"Quantity\" FROM \"Orders\" WHERE \"Food name\" = ?";
             try (PreparedStatement selectPst = con.prepareStatement(selectQuery)) {
+
                 selectPst.setString(1, foodName);
                 ResultSet rs = selectPst.executeQuery();
 
@@ -59,6 +66,7 @@ public class PostgreSQL {
                     updatePst.setString(2, foodName);
 
                     int rowsAffected = updatePst.executeUpdate();
+
                     if (rowsAffected > 0) {
                         System.out.println("Quantity updated successfully!");
                     } else {
@@ -75,7 +83,7 @@ public class PostgreSQL {
     public static void deleteFood(String foodName) {
         String url = "jdbc:postgresql:postgres";
         String user = "postgres";
-        String password = "Adrian";
+        String password = "";
 
         try (Connection con = DriverManager.getConnection(url, user, password)) {
             System.out.println("Connected to PostgreSQL database!");
@@ -104,7 +112,7 @@ public class PostgreSQL {
     public static String readFoodNameFromDatabase(int rowNum) {
         String url = "jdbc:postgresql:postgres";
         String user = "postgres";
-        String password = "Adrian";
+        String password = "jmjavery";
 
         String foodName = null;
         try (Connection con = DriverManager.getConnection(url, user, password)) {
@@ -133,7 +141,7 @@ public class PostgreSQL {
     public static double readPriceFromDatabase(int rowNum) {
         String url = "jdbc:postgresql:postgres";
         String user = "postgres";
-        String password = "Adrian";
+        String password = "";
 
         double price = 0.0;
         try (Connection con = DriverManager.getConnection(url, user, password)) {
@@ -162,7 +170,7 @@ public class PostgreSQL {
     public static int readQuantityFromDatabase(int rowNum) {
         String url = "jdbc:postgresql:postgres";
         String user = "postgres";
-        String password = "Adrian";
+        String password = "";
 
         int quantity = 0;
         try (Connection con = DriverManager.getConnection(url, user, password)) {
@@ -191,7 +199,7 @@ public class PostgreSQL {
     public static int getRowCount() {
         String url = "jdbc:postgresql:postgres";
         String user = "postgres";
-        String password = "Adrian";
+        String password = "";
 
         int rowCount = 0;
         try (Connection con = DriverManager.getConnection(url, user, password)) {
